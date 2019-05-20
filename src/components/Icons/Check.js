b78fb73fe4a36ce77b934bb2c7e95e68fbca2390
JulieSemenova <julie.loggi@gmail.com>
2019-05-20 17:00:47 +0300

    🚨 remove linter errors

--- src/components/Icons/Check.js
+++ src/components/Icons/Check.js
@@ -5,7 +5,7 @@ const Check = props => (
   <svg width="9" height="10" viewBox="0 0 9 10">
     <path
       fill="none"
-      fill-rule="evenodd"
+      fillRule="evenodd"
       stroke={props.color}
       d="M2.814 9.17L8.25 1.816v-.29L2.84 8.902.5 5.894v.277l2.314 3z"
     />
--- src/components/Icons/QuestionMessage.js
+++ src/components/Icons/QuestionMessage.js
@@ -5,7 +5,7 @@ const QuestionMessage = props => (
   <svg viewBox="0 0 40 40">
     <path
       fill={props.color}
-      fill-rule="nonzero"
+      fillRule="nonzero"
       d="M36 0H4C1.8 0 .02 1.8.02 4L0 40l8-8h28c2.2 0 4-1.8 4-4V4c0-2.2-1.8-4-4-4zM22 24h-4v-4h4v4zm0-8h-4V8h4v8z"
     />
   </svg>
