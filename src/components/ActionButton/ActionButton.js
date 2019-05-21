51ae15fa2b7b8f4da6825cc919da285298da74a9
JulieSemenova <julie.loggi@gmail.com>
2019-05-21 19:55:53 +0300

    ✨action button component

---
+++ src/components/ActionButton/ActionButton.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import Icon from '../../components/Icon/Icon';
+import './ActionButton.css';
+
+class ActionButton extends Component {
+  state = {
+    isVisible: false,
+  };
+
+  toggleButton = () => {
+    this.setState({
+      isVisible: !this.state.isVisible,
+    });
+  };
+  render() {
+    const { state, className } = this.props;
+    const { isVisible } = this.state;
+
+    return (
+      <div className={['action', className].join(' ')} onClick={this.toggleButton}>
+        <Icon
+          className={`action_btn ${isVisible ? 'action_btn--active' : ''}`}
+          name="dots"
+          color="#e6ecf2"
+          size={12}
+        />
+        <div class={`action_info ${isVisible ? 'action_info--visible' : ''}`}>
+          {state === 'disabled' ? 'Включить' : 'Выключить'}
+        </div>
+      </div>
+    );
+  }
+}
+
+ActionButton.propTypes = {
+  state: PropTypes.string.isRequired,
+  className: PropTypes.string,
+};
+
+export default ActionButton;
