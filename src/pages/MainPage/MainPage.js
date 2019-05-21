c6e89bf351c7a84dde22240b549d8183549b5c71
JulieSemenova <julie.loggi@gmail.com>
2019-05-21 11:48:23 +0300

    :construction: wip

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 
+import InfoItem from '../../components/InfoItem/InfoItem';
+
 class MainPage extends Component {
   render() {
     return (
       <div>
         <span>Main Page</span>
+        <InfoItem
+          title="Google"
+          syncInfo={{ total: 30 }}
+          rate={{ value: '4.5', feedback: { total: 198, notAnswered: 15 } }}
+          isActive={true}
+        />
       </div>
     );
   }
